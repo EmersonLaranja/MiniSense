a66d584adc62906c9e43c307cce6addad1be90c0
EmersonLaranja <emersonlaranja@gmail.com>
2022-01-15 20:31:04 -0300

    feat: use user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ import { usersRoute } from "./users.routes";
 
 const router = Router();
 
-router.get("/", () => console.log("oi"));
 router.use("/users", usersRoute);
+
+export { router };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 
+import createUserController from "../useCases/createUser";
+
 const usersRoute = Router();
+
+usersRoute.post("/", (req, res) => {
+  return createUserController().handle(req, res);
+});
 
 export { usersRoute };
